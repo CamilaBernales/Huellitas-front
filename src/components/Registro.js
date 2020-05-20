2b9183578d1fd0934b7c886cd78b9457c7f06545
CamilaBernales <camilabernales09@gmail.com>
2020-05-20 16:19:22 -0300

    estilos en login y registro

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -12,74 +12,79 @@ export default function Registro() {
             <img src={registroimg} />
           </div>
           <Col sm={12} md={4}>
-            <Form className={styles.formulario}>
-              <h2 className="text-center text-uppercase mx-4">
-                Crear una cuenta
-              </h2>
-              <Form.Group controlId="formName">
-                <Form.Label className="d-flex justify-content-start">
-                  Nombre:
-                </Form.Label>
-                <Form.Control 
-                className="border border-warning rounded-left"
-                type="text" 
-                placeholder="Ingrese su nombre" />
-              </Form.Group>
+            <div className={styles.formulario}>
+              <Form>
+                <h2 className="text-center text-uppercase mx-4">
+                  Crear una cuenta
+                </h2>
+                <Form.Group controlId="formName">
+                  <Form.Label className="d-flex justify-content-start">
+                    Nombre:
+                  </Form.Label>
+                  <Form.Control
+                    className="border border-warning rounded-left"
+                    type="text"
+                    placeholder="Ingrese su nombre"
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formLastName">
-                <Form.Label className="d-flex justify-content-start">
-                  Apellido:
-                </Form.Label>
-                <Form.Control 
-                className="border border-warning rounded-left"
-                type="text" 
-                placeholder="Ingrese su apellido" />
-              </Form.Group>
+                <Form.Group controlId="formLastName">
+                  <Form.Label className="d-flex justify-content-start">
+                    Apellido:
+                  </Form.Label>
+                  <Form.Control
+                    className="border border-warning rounded-left"
+                    type="text"
+                    placeholder="Ingrese su apellido"
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formEmail">
-                <Form.Label className="d-flex justify-content-start">
-                  Email:
-                </Form.Label>
-                <Form.Control 
-                className="border border-warning rounded-left"
-                type="email" 
-                placeholder="Ingrese su email" />
-              </Form.Group>
+                <Form.Group controlId="formEmail">
+                  <Form.Label className="d-flex justify-content-start">
+                    Email:
+                  </Form.Label>
+                  <Form.Control
+                    className="border border-warning rounded-left"
+                    type="email"
+                    placeholder="Ingrese su email"
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formPassword">
-                <Form.Label className="d-flex justify-content-start">
-                  Contraseña:
-                </Form.Label>
-                <Form.Control
-                  className="border border-warning rounded-left"
-                  type="password"
-                  placeholder="Ingrese su contraseña"
-                />
-              </Form.Group>
+                <Form.Group controlId="formPassword">
+                  <Form.Label className="d-flex justify-content-start">
+                    Contraseña:
+                  </Form.Label>
+                  <Form.Control
+                    className="border border-warning rounded-left"
+                    type="password"
+                    placeholder="Ingrese su contraseña"
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formConfirmPassword">
-                <Form.Label className="d-flex justify-content-start">
-                  Confirmar contraseña:
-                </Form.Label>
-                <Form.Control
-                  className="border border-warning rounded-left"
-                  type="password"
-                  placeholder="Ingrese su contraseña nuevamente"
-                />
-              </Form.Group>
+                <Form.Group controlId="formConfirmPassword">
+                  <Form.Label className="d-flex justify-content-start">
+                    Confirmar contraseña:
+                  </Form.Label>
+                  <Form.Control
+                    className="border border-warning rounded-left"
+                    type="password"
+                    placeholder="Ingrese su contraseña nuevamente"
+                  />
+                </Form.Group>
 
-              <Row>
-                <Col className="d-flex justify-content-end">
-                  <Button
-                    className="text-white text-uppercase font-weight-bold rounded-pill btn btn-button w-100"
-                    variant="warning"
-                    type="submit"
-                  >
-                    Enviar
-                  </Button>
-                </Col>
-              </Row>
-            </Form>
+                <Row>
+                  <Col className="d-flex justify-content-end">
+                    <Button
+                      className="text-white text-uppercase font-weight-bold rounded-pill btn btn-button w-100"
+                      variant="warning"
+                      type="submit"
+                    >
+                      Enviar
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </div>
           </Col>
         </Row>
       </Container>
