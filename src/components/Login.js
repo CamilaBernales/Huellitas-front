ae1bb3f75d18656213981d5d8149f5b2ce37461f
CamilaBernales <camilabernales09@gmail.com>
2020-05-20 14:31:19 -0300

    estilos a login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,52 @@
-import React, { Fragment } from 'react';
-import {Container, Form, Button, Col, Row } from 'react-bootstrap';
-import Logo from './Header/Logo';
-import Navbar from './Header/Navbar';
-
+import React, { Fragment } from "react";
+import { Container, Form, Button, Col, Row } from "react-bootstrap";
+import {Link} from 'react-router-dom'
+import styles from "./css/Login.module.css";
+import imgLogin from "./img/login.svg";
 export default function Login() {
   return (
     <Fragment>
-      <Logo />
-      <Navbar />
-      <Container className="m-4">
-        <Row className="justify-content-center">
+      <Container>
+        <Row className="p-5 d-flex justify-content-end align-items-center">
+          <div className="col mx-5 img-fluid">
+            <img src={imgLogin} />
+          </div>
           <Col sm={12} md={4}>
-            <Form >
+            <Form className={styles.formulario}>
+              <h1 className="text-center ">LOGIN</h1>
               <Form.Group controlId="formEmail">
-                <Form.Label className="d-flex justify-content-start">Email:</Form.Label>
-                <Form.Control type="email" placeholder="Ingrese su email" />
+                <Form.Label className="d-flex justify-content-start">
+                  Email:
+                </Form.Label>
+                <Form.Control 
+                type="email" 
+                placeholder="Ingrese su email"
+                className="border border-warning rounded-left" />
               </Form.Group>
-
               <Form.Group controlId="formPassword">
-                <Form.Label className="d-flex justify-content-start">Contraseña:</Form.Label>
-                <Form.Control type="password" placeholder="Ingrese su contraseña" />
+                <Form.Label className="d-flex justify-content-start">
+                  Contraseña:
+                </Form.Label>
+                <Form.Control
+                  className="border border-warning rounded-left"
+                  type="password"
+                  placeholder="Ingrese su contraseña"
+                />
               </Form.Group>
-
               <Row>
-                <Col className="d-flex justify-content-end">
-                  <Button variant="primary" type="submit">
+                <Col className="d-flex justify-content-center">
+                  <Button
+                    className="text-white text-uppercase font-weight-bold rounded-pill btn btn-button w-100"
+                    variant="warning"
+                    type="submit"
+                  >
                     Ingresar
                   </Button>
                 </Col>
               </Row>
+                  <Link className="d-flex justify-content-start p-2" to={'/registro'}>
+                    No tienes una cuenta? Registrate!
+                  </Link>
             </Form>
           </Col>
         </Row>
