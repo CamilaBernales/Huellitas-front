837653890eef31009b9f371218c465d08a0fdd6a
¨Nicolas <nicooriguela@gmail.com>
2020-06-03 15:52:48 -0300

    Soluciono problema en contacto

--- src/components/usuario/Contacto/Contacto.js
+++ src/components/usuario/Contacto/Contacto.js
@@ -1,13 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import Navbar from '../Elementos-Comunes/Navbar'
 import Logo from '../Elementos-Comunes/Logo'
 import Footer from '../Elementos-Comunes/Footer'
 import { Form, Container, Button, Col, Row, } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import imgLogin from "../../../img/login.svg";
 // import style from '../../../css/Login.module.css'
 
 export default function Contacto() {
+
+    const [consulta, setConsulta] = useState({
+        nombre:'',
+        email:'',
+        mensaje:''
+    })
+    
+    const {nombre, email, mensaje} = consulta
+
+    const onChangeConsulta = (e) =>{
+        setConsulta({
+            ...consulta,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const onSubmitConsulta = (e) =>{
+        e.preventDefault()
+        if(nombre === '' || email === '' || mensaje === ''){
+            alert('Es obligatorio llenar todos los campos')
+        }
+    }
+
     return (
         <Fragment>
             <Logo />
@@ -18,27 +39,42 @@ export default function Contacto() {
                         <div >
                             <Form>
                                 <h4 className="text-center mx-1 ">MANDANOS UN MENSAJE</h4>
+                                <Form.Group controlId="formName">
+                                    <Form.Label className="d-flex justify-content-start">
+                                    Nombre:
+                                    </Form.Label>
+                                    <Form.Control
+                                    className="border border-warning rounded-left"
+                                    type="name"
+                                    placeholder="Ingrese su nombre completo"
+                                    name="nombre"
+                                    value={nombre}
+                                    onChange={onChangeConsulta}
+                                    />
+                                </Form.Group>
                                 <Form.Group controlId="formEmail">
                                     <Form.Label className="d-flex justify-content-start">
                                     Email:
                                     </Form.Label>
                                     <Form.Control
-                                    type="email"
-                                    placeholder="Ingrese su email"
                                     className="border border-warning rounded-left"
+                                    type="email"
+                                    placeholder="Ingrese su Email"
                                     name="email"
+                                    value={email}
+                                    onChange={onChangeConsulta}
                                     />
                                 </Form.Group>
-                                <Form.Group controlId="formPassword">
-                                    <Form.Label className="d-flex justify-content-start">
-                                    Contraseña:
-                                    </Form.Label>
+                                <Form.Group controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label>Example textarea</Form.Label>
                                     <Form.Control
-                                    className="border border-warning rounded-left"
-                                    type="password"
-                                    placeholder="Ingrese su contraseña"
-                                    name="password"
-                                    />
+                                    className="border border-warning"
+                                    as="textarea"
+                                    rows="3"
+                                    name="mensaje"
+                                    value={mensaje}
+                                    onChange={onChangeConsulta}
+                                       />
                                 </Form.Group>
                                 <Row>
                                     <Col className="d-flex justify-content-center">
@@ -46,8 +82,9 @@ export default function Contacto() {
                                         className="text-white text-uppercase font-weight-bold rounded-pill btn btn-button w-100"
                                         variant="warning"
                                         type="submit"
+                                        onSubmit={onSubmitConsulta}
                                     >
-                                        Ingresar
+                                        Enviar
                                     </Button>
                                     </Col>
                                 </Row>
