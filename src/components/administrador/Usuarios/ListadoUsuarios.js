6c4397c103b3954fe84fc8666a42e127ffb03986
Camila Bernales <camilabernales09@gmail.com>
2020-06-15 20:04:19 -0300

    rutas de administrador y de usuario protegidas

---
+++ src/components/administrador/Usuarios/ListadoUsuarios.js
@@ -0,0 +1,11 @@
+import React,{useState, useEffect} from 'react'
+
+const ListadoUsuarios = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ListadoUsuarios
